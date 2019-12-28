be9c82030bc7bca267f18111dc0f686c3cf87c9e
Paul Tyng <paul@paultyng.net>
2019-12-28 12:12:59 -0500

    Test open security

--- provider/resource_wlan_test.go
+++ provider/resource_wlan_test.go
@@ -6,14 +6,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccWLAN_basic(t *testing.T) {
+func TestAccWLAN_wpapsk(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { preCheck(t) },
 		Providers: providers,
 		// TODO: CheckDestroy: ,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccWLANConfig,
+				Config: testAccWLANConfig_wpapsk,
 				Check:  resource.ComposeTestCheckFunc(
 				// testCheckNetworkExists(t, "name"),
 				),
@@ -26,7 +26,27 @@ func TestAccWLAN_basic(t *testing.T) {
 	})
 }
 
-const testAccWLANConfig = `
+func TestAccWLAN_open(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { preCheck(t) },
+		Providers: providers,
+		// TODO: CheckDestroy: ,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWLANConfig_open,
+				Check:  resource.ComposeTestCheckFunc(
+				// testCheckNetworkExists(t, "name"),
+				),
+			},
+			importStep("unifi_wlan.test",
+				"name", "passphrase", "vlan_id", "wlan_group_id",
+				"user_group_id", "security",
+			),
+		},
+	})
+}
+
+const testAccWLANConfig_wpapsk = `
 data "unifi_wlan_group" "default" {
 }
 
@@ -34,11 +54,27 @@ data "unifi_user_group" "default" {
 }
 
 resource "unifi_wlan" "test" {
-	name          = "foo"
+	name          = "foowpapsk"
 	vlan_id       = 202
 	passphrase    = "12345678"
 	wlan_group_id = data.unifi_wlan_group.default.id
 	user_group_id = data.unifi_user_group.default.id
 	security      = "wpapsk"
+}
+`
+
+const testAccWLANConfig_open = `
+data "unifi_wlan_group" "default" {
+}
+
+data "unifi_user_group" "default" {
+}
+
+resource "unifi_wlan" "test" {
+	name          = "fooopen"
+	vlan_id       = 202
+	wlan_group_id = data.unifi_wlan_group.default.id
+	user_group_id = data.unifi_user_group.default.id
+	security      = "open"
 }
 `
