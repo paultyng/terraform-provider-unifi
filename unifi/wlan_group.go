6b8f3f76aa5a19d1a4be5dd0b30b26ffc6e29225
Paul Tyng <paul@paultyng.net>
2019-12-30 04:12:31 -0500

    Add additional API for WLAN groups

--- unifi/wlan_group.go
+++ unifi/wlan_group.go
@@ -16,7 +16,11 @@ type WLANGroup struct {
 
 	BSupported         bool   `json:"b_supported"`
 	LoadBalanceEnabled bool   `json:"loadbalance_enabled"`
-	PMFMode            string `json:"pmf_mode"`
+	PMFMode            string `json:"pmf_mode"` // "disabled", "optional", "required"
+	// roam_channel_na: 36
+	// roam_channel_ng: 1
+	// roam_enabled: false
+	// roam_radio: "ng"
 }
 
 func (c *Client) ListWLANGroup(site string) ([]WLANGroup, error) {
@@ -31,4 +35,71 @@ func (c *Client) ListWLANGroup(site string) ([]WLANGroup, error) {
 	}
 
 	return respBody.Data, nil
+}
+
+func (c *Client) GetWLANGroup(site, id string) (*WLANGroup, error) {
+	var respBody struct {
+		Meta meta        `json:"meta"`
+		Data []WLANGroup `json:"data"`
+	}
+
+	err := c.do("GET", fmt.Sprintf("s/%s/rest/wlangroup/%s", site, id), nil, &respBody)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(respBody.Data) != 1 {
+		return nil, &NotFoundError{}
+	}
+
+	d := respBody.Data[0]
+	return &d, nil
+}
+
+func (c *Client) DeleteWLANGroup(site, id string) error {
+	err := c.do("DELETE", fmt.Sprintf("s/%s/rest/wlangroup/%s", site, id), struct{}{}, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) CreateWLANGroup(site string, d *WLANGroup) (*WLANGroup, error) {
+	var respBody struct {
+		Meta meta        `json:"meta"`
+		Data []WLANGroup `json:"data"`
+	}
+
+	err := c.do("POST", fmt.Sprintf("s/%s/rest/wlangroup", site), d, &respBody)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(respBody.Data) != 1 {
+		return nil, &NotFoundError{}
+	}
+
+	new := respBody.Data[0]
+
+	return &new, nil
+}
+
+func (c *Client) UpdateWLANGroup(site string, d *WLANGroup) (*WLANGroup, error) {
+	var respBody struct {
+		Meta meta        `json:"meta"`
+		Data []WLANGroup `json:"data"`
+	}
+
+	err := c.do("PUT", fmt.Sprintf("s/%s/rest/wlangroup/%s", site, d.ID), d, &respBody)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(respBody.Data) != 1 {
+		return nil, &NotFoundError{}
+	}
+
+	new := respBody.Data[0]
+
+	return &new, nil
 }
