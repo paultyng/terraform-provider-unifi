b028176138cc857b6c4b88bbe915a31ea7ad49cb
Paul Tyng <paul@paultyng.net>
2020-01-11 20:06:14 -0500

    Setup http client manually

--- internal/provider/lazy_client.go
+++ internal/provider/lazy_client.go
@@ -21,36 +21,39 @@ type lazyClient struct {
 	inner *unifi.Client
 }
 
+func setHTTPClient(c *unifi.Client) {
+	httpClient := &http.Client{}
+	httpClient.Transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+
+		// TODO: make this opt-in
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	httpClient.Transport = logging.NewTransport("Unifi", httpClient.Transport)
+
+	jar, _ := cookiejar.New(nil)
+	httpClient.Jar = jar
+
+	c.SetHTTPClient(httpClient)
+}
+
 func (c *lazyClient) init() error {
 	var err error
 	c.once.Do(func() {
 		c.inner = &unifi.Client{}
-
-		httpClient := &http.Client{}
-		httpClient.Transport = &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-
-			// TODO: make this opt-in
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		}
-
-		httpClient.Transport = logging.NewTransport("Unifi", httpClient.Transport)
-
-		jar, _ := cookiejar.New(nil)
-		httpClient.Jar = jar
-
-		c.inner.SetHTTPClient(httpClient)
+		setHTTPClient(c.inner)
 
 		err = c.inner.SetBaseURL(c.baseURL)
 		if err != nil {
