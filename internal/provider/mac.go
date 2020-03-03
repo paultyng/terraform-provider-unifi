cbf24326d92f85ca8caffb2b84e2c4d60ebc3c43
Paul Tyng <paul@paultyng.net>
2020-03-03 17:01:02 -0500

    Support dashes in MAC

--- internal/provider/mac.go
+++ internal/provider/mac.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-var macAddressRegexp = regexp.MustCompile("^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$")
+var macAddressRegexp = regexp.MustCompile("^([0-9a-fA-F][0-9a-fA-F][-:]){5}([0-9a-fA-F][0-9a-fA-F])$")
 
 func macDiffSuppressFunc(k, old, new string, d *schema.ResourceData) bool {
 	old = strings.TrimSpace(strings.ReplaceAll(strings.ToLower(old), "-", ":"))
