5a04afe165719262082cc3fe60052fc0f0dfc499
Paul Tyng <paul@paultyng.net>
2020-03-03 14:53:43 -0500

    Minor fixes for mac address filtering

---
+++ internal/provider/mac.go
@@ -0,0 +1,16 @@
+package provider
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+var macAddressRegexp = regexp.MustCompile("^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$")
+
+func macDiffSuppressFunc(k, old, new string, d *schema.ResourceData) bool {
+	old = strings.TrimSpace(strings.ReplaceAll(strings.ToLower(old), "-", ":"))
+	new = strings.TrimSpace(strings.ReplaceAll(strings.ToLower(new), "-", ":"))
+	return old == new
+}
