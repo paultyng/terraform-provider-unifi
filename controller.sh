66a71f10f727070871216e6321a6568b58f949fb
Jo Vanvoorden <jo.vanvoorden@telenet.be>
2020-03-03 21:25:37 +0100

    Finetune the controller, make it easier to use

--- controller.sh
+++ controller.sh
@@ -6,6 +6,10 @@ set -eou pipefail
 # Username: tfacctest
 # Password: tfacctest1234
 # Email: tfacctest@example.com
+DOCKER_HTTP_PORT="${DOCKER_HTTP_PORT:-8080}"
+DOCKER_HTTPS_PORT="${DOCKER_HTTPS_PORT:-8443}"
+DOCKER_STUN_PORT="${DOCKER_STUN_PORT:-3478}"
+DOCKER_AIRCONTROL_PORT="${DOCKER_AIRCONTROL_PORT:-10001}"
 
 if test $# -eq 0; then
   echo "please specify either 'start' or 'test'"
@@ -15,26 +19,32 @@ fi
 case "$1" in
   "start")
     docker run --rm --init -d \
-      -p 8080:8080 \
-      -p 8443:8443 \
-      -p 3478:3478/udp \
-      -p 10001:10001/udp \
+      -p ${DOCKER_HTTP_PORT}:8080 \
+      -p ${DOCKER_HTTPS_PORT}:8443 \
+      -p ${DOCKER_STUN_PORT}:3478/udp \
+      -p ${DOCKER_AIRCONTROL_PORT}:10001/udp \
       -e TZ='America/New_York' \
       -v $(pwd)/testdata/unifi:/unifi \
       --name unifi \
       jacobalberty/unifi:stable
 
     echo "Waiting for login page..."
-    timeout 300 bash -c 'while [[ "$(curl --insecure -s -o /dev/null -w "%{http_code}" https://localhost:8443/manage/account/login)" != "200" ]]; do sleep 5; done'
+    timeout 300 bash -c 'while [[ "$(curl --insecure -s -o /dev/null -w "%{http_code}" https://localhost:${DOCKER_HTTPS_PORT}/manage/account/login)" != "200" ]]; do sleep 5; done'
     echo "Controller running."
     ;;
   "test")
     TF_ACC=1 \
     UNIFI_USERNAME=tfacctest \
     UNIFI_PASSWORD=tfacctest1234 \
-    UNIFI_API="https://localhost:8443/api/" \
+    UNIFI_API="https://localhost:${DOCKER_HTTPS_PORT}/api/" \
     UNIFI_ACC_WLAN_CONCURRENCY="4" \
     go test -v -cover ./internal/provider
+    ;;
+  "stop")
+    docker stop unifi
+    ;;
+  "update")
+    docker pull jacobalberty/unifi:stable
     ;;
   *)
     echo "unrecognized command"
