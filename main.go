59ff70bc6c3feb653e5f3ce86907fb2328dda5b9
Ben Davies <ben.davies@ourscienceistight.com>
2020-10-12 08:23:15 +0100

    Removed package name edits

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
-package main // import "github.com/jalfresi/terraform-provider-unifi"
+package main // import "github.com/paultyng/terraform-provider-unifi"
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/jalfresi/terraform-provider-unifi/internal/provider"
+	"github.com/paultyng/terraform-provider-unifi/internal/provider"
 )
 
 func main() {
