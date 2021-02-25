1bd52e139677f099a9267e077dfe620bbefc7862
Andy Alm <andy.alm@gettyimages.com>
2021-02-24 20:42:11 -0800

    Added a test for a firewall rule using dst_port

--- internal/provider/resource_firewall_rule_test.go
+++ internal/provider/resource_firewall_rule_test.go
@@ -23,6 +23,19 @@ func TestAccFirewallRule_basic(t *testing.T) {
 	})
 }
 
+func TestAccFirewallRule_dst_port(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFirewallRuleConfigWithPort,
+			},
+			importStep("unifi_firewall_rule.test"),
+		},
+	})
+}
+
 // func TestAccFirewallRule_firewall_group(t *testing.T) {
 // func TestAccFirewallRule_network(t *testing.T) {
 
@@ -46,6 +59,22 @@ resource "unifi_firewall_rule" "test" {
 	src_firewall_group_ids = [unifi_firewall_group.test.id]
 
 	dst_address = "192.168.1.1"
+}
+`
+
+const testAccFirewallRuleConfigWithPort = `
+resource "unifi_firewall_rule" "test" {
+	name    = "tf acc"
+	action  = "accept"
+	ruleset = "LAN_IN"
+
+	rule_index = 2010
+
+	protocol = "tcp"
+
+	src_address = "192.168.3.3"
+	dst_address = "192.168.1.1"
+	dst_port    = 53
 }
 `
 
