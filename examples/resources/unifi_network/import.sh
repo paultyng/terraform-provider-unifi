f21eb3fd1e275e950555d560b45611657faf8099
Paul Tyng <paul@paultyng.net>
2021-03-21 16:08:10 -0400

    Add example to shell file for generation

--- examples/resources/unifi_network/import.sh
+++ examples/resources/unifi_network/import.sh
@@ -3,3 +3,6 @@ terraform import unifi_network.mynetwork 5dc28e5e9106d105bdc87217
 
 # import from another site
 terraform import unifi_network.mynetwork bfa2l6i7:5dc28e5e9106d105bdc87217
+
+# import network by name
+terraform import unifi_network.mynetwork name=LAN
