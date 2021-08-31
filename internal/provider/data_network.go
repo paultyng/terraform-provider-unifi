0a8c9dda10571a389b5e0e46dd1e30a928356812
Paul Tyng <paul@paultyng.net>
2021-08-30 21:14:11 -0400

    Add support for unifi_network datasource
    #168

---
+++ internal/provider/data_network.go
@@ -0,0 +1,62 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataNetwork() *schema.Resource {
+	return &schema.Resource{
+		Description: "`unifi_network` data source can be used to retrieve the ID for a network by name.",
+
+		ReadContext: dataNetworkRead,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Description: "The ID of this AP group.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"site": {
+				Description: "The name of the site the user group is associated with.",
+				Type:        schema.TypeString,
+				Computed:    true,
+				Optional:    true,
+			},
+			"name": {
+				Description: "The name of the user group to look up.",
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "Default",
+			},
+		},
+	}
+}
+
+func dataNetworkRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*client)
+
+	name := d.Get("name").(string)
+	site := d.Get("site").(string)
+	if site == "" {
+		site = c.site
+	}
+
+	networks, err := c.c.ListNetwork(ctx, site)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	for _, n := range networks {
+		if n.Name == name {
+			d.SetId(n.ID)
+
+			d.Set("site", site)
+
+			return nil
+		}
+	}
+
+	return diag.Errorf("network not found with name %s", name)
+}
