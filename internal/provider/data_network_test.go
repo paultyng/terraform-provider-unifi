1abc3f5e2175ddc601ddf252d20898c6eb7c61ca
Tommi2Day <github@tdressler.net>
2021-09-07 20:41:44 +0200

    allow using id or name for unifi_network data source Add sample and update docs along
    implements #174

--- internal/provider/data_network_test.go
+++ internal/provider/data_network_test.go
@@ -6,14 +6,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccDataNetwork_default(t *testing.T) {
+func TestAccDataNetwork_byName(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { preCheck(t) },
 		ProviderFactories: providerFactories,
 		// TODO: CheckDestroy: ,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataNetworkConfig_default,
+				Config: testAccDataNetworkConfig_byName,
 				Check:  resource.ComposeTestCheckFunc(
 				// testCheckNetworkExists(t, "name"),
 				),
@@ -22,8 +22,34 @@ func TestAccDataNetwork_default(t *testing.T) {
 	})
 }
 
-const testAccDataNetworkConfig_default = `
+func TestAccDataNetwork_byID(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: providerFactories,
+		// TODO: CheckDestroy: ,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataNetworkConfig_byID,
+				Check:  resource.ComposeTestCheckFunc(
+				// testCheckNetworkExists(t, "name"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataNetworkConfig_byName = `
 data "unifi_network" "lan" {
 	name = "LAN"
+}
+`
+
+const testAccDataNetworkConfig_byID = `
+data "unifi_network" "lan" {
+	name = "LAN"
+}
+
+data "unifi_network" "lan_id" {
+	id = data.unifi_network.lan.id
 }
 `
