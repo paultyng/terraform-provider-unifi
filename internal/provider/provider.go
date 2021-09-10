309b3aaacab50c8914ed80c9560b43130d1e8469
Paul Tyng <paul@paultyng.net>
2021-09-10 09:44:12 -0400

    Add WPA3 support to unifi_wlan
    Fixes #154

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -15,6 +15,9 @@ import (
 var (
 	controllerV5 = version.Must(version.NewVersion("5.0.0"))
 	controllerV6 = version.Must(version.NewVersion("6.0.0"))
+
+	// https://community.ui.com/releases/UniFi-Network-Controller-6-1-61/62f1ad38-1ac5-430c-94b0-becbb8f71d7d
+	controllerVersionWPA3 = version.Must(version.NewVersion("6.1.61"))
 )
 
 func init() {
