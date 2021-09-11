f7752337bd5e4b131c006de74816fd211d08dc8e
Paul Tyng <paul@paultyng.net>
2021-09-11 13:38:27 -0400

    Move version code to separate files

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -82,30 +81,6 @@ func preCheck(t *testing.T) {
 		if value == "" {
 			t.Fatalf("`%s` must be set for acceptance tests!", variable)
 		}
-	}
-}
-
-func preCheckMinVersion(t *testing.T, min *version.Version) {
-	v, err := version.NewVersion(testClient.Version())
-	if err != nil {
-		t.Fatalf("error parsing version: %s", err)
-	}
-	if v.LessThan(min) {
-		t.Skipf("skipping test on controller version %q (need at least %q)", v, min)
-	}
-}
-
-func preCheckV6Only(t *testing.T) {
-	preCheckMinVersion(t, controllerV6)
-}
-
-func preCheckV5Only(t *testing.T) {
-	v, err := version.NewVersion(testClient.Version())
-	if err != nil {
-		t.Fatalf("error parsing version: %s", err)
-	}
-	if v.GreaterThanOrEqual(controllerV6) {
-		t.Skipf("skipping test on controller version %q", v)
 	}
 }
 
