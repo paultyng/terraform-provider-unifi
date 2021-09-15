81bdfc0ffead8ee1873bcecb381cac08a336e146
Paul Tyng <paul@paultyng.net>
2021-09-14 21:18:25 -0400

    Minor formatting fixes

--- internal/provider/resource_site.go
+++ internal/provider/resource_site.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/paultyng/go-unifi/unifi"
 )
