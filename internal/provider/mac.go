d550ae4a590eac0ebb029c4644bf73ea1c88529d
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2025-01-03 15:16:15 +0100

    fix: generate random name and mac in TestAccDataAccount_default and TestAccDataAccount_mac respectively to fix flakiness
    References #480

--- internal/provider/mac.go
+++ internal/provider/mac.go
@@ -1,8 +1,12 @@
 package provider
 
 import (
+	mapset "github.com/deckarep/golang-set/v2"
+	"net"
 	"regexp"
 	"strings"
+	"sync"
+	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -17,4 +21,34 @@ func macDiffSuppressFunc(k, old, new string, d *schema.ResourceData) bool {
 	old = cleanMAC(old)
 	new = cleanMAC(new)
 	return old == new
+}
+
+var (
+	macInit sync.Once
+	macPool = mapset.NewSet[*net.HardwareAddr]()
+)
+
+func allocateTestMac(t *testing.T) (string, func()) {
+	macInit.Do(func() {
+		// for test MAC addresses, see https://tools.ietf.org/html/rfc7042#section-2.1.
+		for i := 0; i < 512; i++ {
+			mac := net.HardwareAddr{0x00, 0x00, 0x5e, 0x00, 0x53, byte(i)}
+			if ok := macPool.Add(&mac); !ok {
+				t.Fatal("Failed to add MAC to pool")
+			}
+		}
+	})
+
+	mac, ok := macPool.Pop()
+	if mac == nil || !ok {
+		t.Fatal("Unable to allocate test MAC")
+	}
+
+	unallocate := func() {
+		if ok := macPool.Add(mac); !ok {
+			t.Fatal("Failed to add MAC to pool")
+		}
+	}
+
+	return mac.String(), unallocate
 }
--- internal/provider/resource_user_test.go
+++ internal/provider/resource_user_test.go
@@ -6,11 +6,9 @@ import (
 	"net"
 	"regexp"
 	"strings"
-	"sync"
 	"testing"
 
 	"github.com/apparentlymart/go-cidr/cidr"
-	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/paultyng/go-unifi/unifi"
@@ -18,36 +16,6 @@ import (
 
 func userImportStep(name string) resource.TestStep {
 	return importStep(name, "allow_existing", "skip_forget_on_destroy")
-}
-
-var (
-	macInit sync.Once
-	macPool mapset.Set[*net.HardwareAddr] = mapset.NewSet[*net.HardwareAddr]()
-)
-
-func allocateTestMac(t *testing.T) (string, func()) {
-	macInit.Do(func() {
-		// for test MAC addresses, see https://tools.ietf.org/html/rfc7042#section-2.1.
-		for i := 0; i < 256; i++ {
-			mac := net.HardwareAddr{0x00, 0x00, 0x5e, 0x00, 0x53, byte(i)}
-			if ok := macPool.Add(&mac); !ok {
-				t.Fatal("Failed to add MAC to pool")
-			}
-		}
-	})
-
-	mac, ok := macPool.Pop()
-	if mac == nil || !ok {
-		t.Fatal("Unable to allocate test MAC")
-	}
-
-	unallocate := func() {
-		if ok := macPool.Add(mac); !ok {
-			t.Fatal("Failed to add MAC to pool")
-		}
-	}
-
-	return mac.String(), unallocate
 }
 
 func TestAccUser_basic(t *testing.T) {
